5fc9f65df680337780a4d32d015bca78ee6510cf
Kianmes <88176739+KianmesMoon@users.noreply.github.com>
2026-05-26 12:01:00 +0800

    feat(security): configure helmet headers

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import { Elysia } from "elysia";
 import { ENV } from "./config/env";
 import { betterAuth } from "./plugins/better-auth";
 import cors from "./plugins/cors";
+import helmet from "./plugins/helmet";
 import openapi from "./plugins/openapi";
 
-const app = new Elysia().use(betterAuth).use(openapi).use(cors).listen(ENV.PORT);
+const app = new Elysia().use(openapi).use(cors).use(helmet).use(betterAuth).listen(ENV.PORT);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}
---
+++ src/plugins/helmet.ts
@@ -0,0 +1,3 @@
+import { helmet } from "elysia-helmet";
+
+export default helmet();
